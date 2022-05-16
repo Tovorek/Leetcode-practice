d08e3697bf2afdf40eba4beb9a3c4b876cab3e7d
Arrow <71452112+buhuhaha@users.noreply.github.com>
2022-05-16 11:24:35 +0530

    python solution

---
+++ 217 Contains Duplicate.py
@@ -0,0 +1,9 @@
+class Solution(object):
+      Def ContainsDuplicate(self, nums):
+           hashset = set()
+      for n in nums:
+          if n in hashset:
+              return True
+          hashset.add(n)
+      return False    
+          
